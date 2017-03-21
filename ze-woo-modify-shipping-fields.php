ecfaf963c72ddd263faf3ea112a84fad579f1b08
Hosea Baker <hosea.baker@gmail.com>
2017-03-20 23:00:43 -0500

    Base Plugin Files

---
+++ ze-woo-modify-shipping-fields.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Ze Woo Modify Shipping Fields
+ * Plugin URI:      https://hoseabaker.com
+ * Description:     WooCommerce plugin to modify shipping fields
+ * Author:          Hosea Baker
+ * Author URI:      https://hoseabaker.com
+ * Text Domain:     ze-woo-modify-shipping-fields
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ze_Woo_Modify_Shipping_Fields
+ */
